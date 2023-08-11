b933850eb82c80d34507e292b00c7711b2f50a18
shameemahsan1 <shameem8590gmail.com>
2023-08-11 12:46:24 +0600

    html page website

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var x = setInterval(function(){
     var distance = countdownDate - nowDT;
 
     // Time calculations for days, hours, munutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var days = Math.floor(distance / (100000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000* 60)) / 1000);
